0603a48caf9d38afa9479407a271d3851f614cc0
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-22 18:14:59 +0530

    blog-app-database

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -17,8 +17,8 @@ export class Services{
     async createPost ({slug, title , content,  featuredImge, status, userId}) {
         try {
             return this.databases.createDocument(
-                conf.appwriteCollectionid,
                 conf.appwriteDatabaseid,
+                conf.appwriteCollectionid,
                 slug,
                 [
                   title,
@@ -34,11 +34,11 @@ export class Services{
         }
     }
     
-    async updatePost ({slug, title , content,  featuredImge, status, userId}){
+    async updatePost (slug,{ title , content,  featuredImge, status, userId}){
         try {
             return this.databases.updateDocument(
-                conf.appwriteCollectionid,
                 conf.appwriteDatabaseid,
+                conf.appwriteCollectionid,
                 slug,
                 [
                   title,
@@ -54,6 +54,81 @@ export class Services{
         }
     }
 
+    async deletePost (slug){
+        try {
+            return this.databases.deleteDocument(
+                conf.appwriteDatabaseid,
+                conf.appwriteCollectionid,
+                slug,
+                
+            )
+            return true
+        } catch (error) {
+            console.log(error);
+            return false
+        }
+    }
+
+    async getPost (slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseid,
+                conf.appwriteCollectionid,
+                slug,
+            )
+        } catch (error) {
+           console.log(error) 
+        }
+    }
+
+    async getPosts(queries =[Query.equal("status", "active")]){
+            try {
+                return await this.databases.listDocuments(
+                    conf.appwriteCollectionid,
+                    conf.appwriteDatabaseid,
+                    queries,
+                    
+                )
+            } catch (error) {
+                console.log(error);
+                return false
+            }
+    }
+// file upload
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+            conf.appwriteBucketid,
+            file    
+
+            )
+        } catch (error) {
+            console.log(error);
+            return false
+        }
+    }
+
+    // delete file 
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+            conf.appwriteBucketid,
+            fileId    
+
+            )
+            return true
+        } catch (error) {
+            console.log(error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+      return this.bucket.getFilePreview(
+        conf.appwriteBucketid,
+            fileId 
+      )  
+    }
 }
 
 
