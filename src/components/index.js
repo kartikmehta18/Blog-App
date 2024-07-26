ff053b48bd08dbcddfdd9b0a54dbc16efd14f5d6
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-26 17:39:13 +0530

    blog-appadd pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login"
+import PostForm from "./post-form/PostForm";
+import Post from "./Post"
 export {Header
-    ,Footer ,Container, Logo, RTE}
+    ,Footer ,Container, Logo, RTE,Signup, Login, PostForm}
