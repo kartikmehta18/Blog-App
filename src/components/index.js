b32142329c53b03552c1ea7ba321c9f01ae80bd5
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-26 12:36:43 +0530

    blog-appadd text editor RTE

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import RTE from "./RTE";
 export {Header
-    ,Footer ,Container, Logo}
+    ,Footer ,Container, Logo, RTE}
